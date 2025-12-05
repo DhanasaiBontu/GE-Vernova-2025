ffe9e28226d83acd6127561af255d26db25633f4
DhanasaiBontu <bdhanasaichowdary@gmail.com>
2025-12-06 00:35:59 +0530

    UC2 - Daily Wage Calculation

--- src/employeewagecomputation/EmployeeWageComputation.java
+++ src/employeewagecomputation/EmployeeWageComputation.java
@@ -1,11 +1,10 @@
 package employeewagecomputation;
 
-// ===== UC1: Check Employee is Present or Absent =====
-
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
+        // ===== UC1: Check Employee is Present or Absent =====
         int random=(int)(Math.random() * 2); // gives 0 or 1
         if(random==1){
             System.out.println("Employee is Present");
@@ -13,5 +12,18 @@ public class EmployeeWageComputation {
         else{
             System.out.println("Employee is Absent");
         }
+
+        // UC2 - Daily Wage Calculation
+        int wagePerHour=20;
+        int fullDayHours=8;
+        int dailyWage = 0;
+
+        if(random==1) {
+            dailyWage=wagePerHour*fullDayHours;
+        }
+        else {
+            dailyWage = 0;
+        }
+        System.out.println("Daily Employee Wage: " + dailyWage);
     }
 }
